37d3302ea923335108c3ba0280442946068c59d9
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-06-05 19:43:25 -0300

    controllers de update sendo consertados

--- src/controllers/updateAlunoSenha.js
+++ src/controllers/updateAlunoSenha.js
@@ -6,18 +6,18 @@ const insertUser = require("../models/InsertUser");
 
 const updateAlunoSenha = async (req, res) => {
 
-    const userFind = await insertUser.findOne({where:{
+    const userFind = await insertUser.findOne({
         email : req.body.emailRecuperacaoSenha
-    }}).then(async () => {
+    }).then(async () => {
 
         if (userFind === null) {
             return res.status(400).send('user not found')
         } else {
-            await userFind.update({
+            await insertUser.update({
                 senha: req.body.novaSenha
-            } , {
+            },{
                 where: {
-                    email: req.body.emailRecuperacaoSenha
+                    email : req.body.emailRecuperacaoSenha
                 }
             })
             res.redirect("/Aluno/login");
