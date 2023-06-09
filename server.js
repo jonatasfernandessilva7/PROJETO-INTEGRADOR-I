5ae97698902515d2c79b8e6add4e50f8c238a7e3
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-06-09 11:03:48 -0300

    webscket connection está executando resolvendo issue #10 e #11

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 //define consts
 const express = require('express');
 const app = express();
+
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require("express-session");
 const flash = require("connect-flash");
+
 require("dotenv").config();
 const port = process.env.PORT_SERVER|| 5000;
+
 const ejs = require("ejs");
 
-//socket io
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
@@ -49,9 +51,21 @@ app.set('view engine', 'ejs');
 var userRoute = require('./src/routes/userRoutes');
 app.use('/', userRoute);
 
-io.on('connection', (socket) => { console.log(socket.id) });
+//socket
+
+const users = [];
+io.on('connection', (socket) => {
+    console.log('nova conexão', socket.id);
+    users.push(socket);
+
+    socket.on('disconnect', function(){
+        users.slice(users.indexOf(socket), 1);
+        console.log('desconectou');
+    });
+});
+
 
 //open server
 server.listen(port, () => {
-    console.log('this is ok!');
+    console.log('rodando na porta: ',port);
 });
